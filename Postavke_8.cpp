8d9b2c32b2fdb42d359c819233d67fe989b0a1ab
Edin <medin932@gmail.com>
2021-02-07 18:25:45 +0100

    Create Postavke_8.cpp

---
+++ Postavke_8.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+Napišite program, poštujući sve faze procesa programiranja, koji će ispisati sve troznamenkaste brojeve koji su jednaki sumi faktorijela svojih znamenki – ABC = (A + B)! + C! Upotrijebite funkcije:
+
+bool provjera (int);
+int faktorijel(int);
+*/
+
+bool provjera(int);
+int faktorijel(int);
+
+int main()
+{
+	for (int i = 100; i < 1000; i++) {
+		if (provjera(i)) {
+			cout << i << "\n";
+		}
+	}
+}
+
+bool provjera(int n) {
+	int trecaCifra = n % 10;
+	int drugaCifra = n / 10 % 10;
+	int prvaCifra = n / 10 / 10 % 10;
+
+	if ((faktorijel(prvaCifra + drugaCifra) + faktorijel(trecaCifra)) == n) {
+		return true;
+	}
+	return false;
+}
+
+int faktorijel(int n) {
+	if (n == 1 || n == 0) {
+		return 1;
+	}
+	return n * faktorijel(n - 1);
+}
